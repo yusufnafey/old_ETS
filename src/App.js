e31e0b4cff3f0fbec1c187f47c7a6407c1661975
Yusuf <yusufnafey@yahoo.com>
2020-02-05 22:55:23 -0800

    adds under construction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './App.scss';
 import './App.css';
-import './Navbar'
-import './Footer'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div class="under-construction-container">
+        <h1 class="under-construction-title">🚧UNDER CONSTRUCTION🚧</h1>
+        <h4 class="under-construction-subtitle">(but feel free to browse, ignore the mess 😅)</h4>
+      </div>
         <Navbar />
         <p>
 Easy ATM Solutions
